2b136f4518a48f83cfb51743d86e34dcbf1e1ab5
sanjay kumar k p <49227865+sanjay-3129@users.noreply.github.com>
2020-11-03 20:28:42 +0530

    Create Solution.java

---
+++ java/warmup1/startOz/Solution.java
@@ -0,0 +1,10 @@
+public String startOz(String str) {
+  String res = "";
+  if(str.length()>=1 && str.charAt(0)=='o'){
+    res = res + str.charAt(0);
+  }
+  if(str.length()>=2 && str.charAt(1)=='z'){
+    res = res + str.charAt(1);
+  }
+  return res;
+}
